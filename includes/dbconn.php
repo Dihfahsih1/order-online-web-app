a216de27dcff5d2fad9ad5e7c81c85bfe2861aed
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-08-26 14:54:15 +0300

    changed configure file to the database connections

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,9 +1,15 @@
 <?php
-/* Database config */
+
+$db_host		= 'localhost';
+$db_user		= 'root';
+$db_pass		= '';
+$db_database	= 'dbcnp';
+
+/* Database config
 $db_host		= 'localhost';
 $db_user		= 'id7489310_eazicakes';
 $db_pass		= 'namonoritah2018';
-$db_database	= 'id7489310_dbcnp'; 
+$db_database	= 'id7489310_dbcnp';
 /* End config */
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
@@ -11,5 +17,3 @@ $con = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
